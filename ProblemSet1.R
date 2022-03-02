462997461fb152c3d2b1f6dcabf224e0d9945125
Sirisha Gadepalli <43031645+Siri-7@users.noreply.github.com>
2022-03-01 19:54:01 -0600

    Delete ProblemSet1.R

--- ProblemSet1.R
+++
@@ -1,8 +0,0 @@
-codebook_data <- read.csv("~/R/codebook.csv")
-head(data)
-
-historic_data <-  read.csv("~/R/historic_property_data.csv")
-head(historic_data)
-
-predict_data <-  read.csv("~/R/predict_property_data.csv")
-head(predict_data)
